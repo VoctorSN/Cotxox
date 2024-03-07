8a9f898e0074e88356157b1f7ff7b5e0e3b18cb8
Victor Clase <victorsancheznogueira@gmail.com>
2024-03-07 12:18:16 +0100

    Initial Commit

---
+++ src/main/java/edu/badpals/cotxox/conductores/Conductor.java
@@ -0,0 +1,14 @@
+package edu.badpals.cotxox.conductores;
+
+import java.util.ArrayList;
+
+public class Conductor {
+    private String nombre;
+    private String modelo;
+    private String matricula;
+    private double valoracionMedia;
+    private boolean ocupado;
+    private ArrayList<Byte> valoraciones;
+
+
+}
